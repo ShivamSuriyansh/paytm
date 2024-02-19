cccbdc62c0af0f0b45050e3b81c661900cfef324
shivam <suriyansh18@gmail.com>
2024-02-19 16:00:50 +0530

    adding account.js route to handle all the balance related api requests

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userRouter = require("./user");
+const accountRouter = require("../routes/account");
 
 const router = express.Router();
 
 router.use("/user", userRouter)
+router.user("/Account",accountRouter);
 
 module.exports = router;
