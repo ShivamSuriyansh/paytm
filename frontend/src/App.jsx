4a07e03bfe6ce30de909a290109d3969f09d2070
shivam <suriyansh18@gmail.com>
2024-02-21 01:31:54 +0530

    initialised frontend and created signUp component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
-
+import './App.css'
+import {BrowserRouter , Routes,Route} from 'react-router-dom';
+import Signup from './components/Signup/Signup.jsx';
+import Signin from './components/Signin/Signin.jsx';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
+import SendMoney from './components/SendMoney/SendMoney.jsx';
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+  </BrowserRouter>
   )
 }
 
